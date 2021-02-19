18fd12538e218af910385f79ebb474119c8fea86
Taina Vilela Bar <67959201+TainaVilela@users.noreply.github.com>
2021-02-19 16:51:31 -0300

    Add files via upload
    Telas, rotas .

---
+++ main.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+const mainController = require("../controllers/main")
+
+/* GET learn page. */
+router.get('/learn', mainController.learn)
+
+/* GET teach page. */
+router.get('/teach', mainController.teach)
+
+/* GET config page */
+router.get('/config', mainController.config)
+
+/* GET delete page */
+router.get('/delete', mainController.delete)
+
+/*GET Create new class */
+router.get('/create', mainController.create)
+
+module.exports = router;
